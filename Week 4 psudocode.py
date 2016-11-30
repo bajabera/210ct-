45f2e6ceefb512aabd1fccd4b5848a3eb47f10c3
bajabera <bajabera@uni.coventry.ac.uk>
2016-11-30 21:35:56 +0000

    Update Week 4 psudocode.py

--- Week 4 psudocode.py
+++ Week 4 psudocode.py
@@ -3,7 +3,7 @@ BINARY_SEARCH(value):
     numbers<-empty list to be searched through later
     range1<-start point of range
     range2<-end point of range
-    r<-length from value of range1 to value of range2
+    r<-range from value of range1 to value of range2
 
     while length(numbers)<size:
         ask<-Take in list of numbers
@@ -13,27 +13,21 @@ BINARY_SEARCH(value):
     listSize<-length of list numbers
     last<-listSize
 
-
+    found=False
     while(first<=last):
         mid<-(first+last)//2
 
-        if value=the middle value of the list numbers
-            print "Value found"
-
-            if that same value is in r
-                print "Value whithin range"
-                return True
-            else
-                print "Vaue not within range"
-                return False
-            return True
-
-        else if value>the middle value of the list numbers
+        if the middle number from list numbers is in r
+            then their is a value in the range
+            found<-True
+            break the while loop
+        else if the middle number is lower than the first range1
             first<-mid+1
-        else if value<the middle value of the list numbers
-            last<-mid-1
         else
-            print "Value not found"
-            return False
-        
+            last<-mid-1
+
+    if found:
+        print('yes')
+    else:
+        print('no')
             
