2b3f924d4e634494cd2787ebcd83e13862a0383d
bajabera <bajabera@uni.coventry.ac.uk>
2016-11-30 22:18:46 +0000

    Update Week 3 number 1.py

--- Week 3 number 1.py
+++ Week 3 number 1.py
@@ -6,13 +6,15 @@ def reverse():
     '''
     try:
     
-        sentence=input("Please enter a sentence ")
-        split1=sentence.split()
+        sentence=input("Please enter a sentence ")#1
+        split1=sentence.split()#1
 
-        for i in split1[::-1]:#extended slices. 3rd argument -1 reverses the sentence
-            print (i)
+        for i in split1[::-1]:#extended slices. 3rd argument -1 reverses the sentence#n
+            print (i)#n
     except Error:
-        print('that is not the correct type of input')
+        print('that is not the correct type of input')#1
 reverse()    
     
 
+#1+1+n+n+1=2n+3
+#O(n)
