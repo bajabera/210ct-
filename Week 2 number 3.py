f02d33998de52ab8529c90e44275bb758938188b
bajabera <bajabera@uni.coventry.ac.uk>
2016-12-01 11:25:02 +0000

    Update Week 2 number 3.py

--- Week 2 number 3.py
+++ Week 2 number 3.py
@@ -4,18 +4,20 @@ n<-(Matrix as a list)#1
 p<-(product Matrix)#1
 
 MATRIX ADDITION(m,n)
-    m[i]+n[i] for i 1<- to length(m)# 
-
+     for i 1<- to length(m)#n
+         m[i]+n[i]
+     return p[i]#n
 SUBTRACTION(m,n)
-    m[i]-n[i] for i 1<- to lenth(m)
-
+     for i 1<- to lenth(m)#n
+         m[i]-n[i]
+     return p[i]#n
 MATRIX MULTIPLICATION(m,n)
 //Takes 2 matrices as input and multiplies each row by corresponding column
      for each row value of m#n
          for each column value of n#n*n
              for position in final matrix#n*n*n
-                 p[i]=(a[i]*b[i])+(a[i+1]*b[i+size of matrix])
-                 return p
+                 p[i]=(m[i]*n[i])+(m[i+1]*n[i+size of matrix])
+    return p#n
 
 MULTIPLY BY INTEGER(x,p)
 //Takes an integer "x" and multiplies by a matrix "p"
@@ -24,7 +26,9 @@ MULTIPLY BY INTEGER(x,p)
 
 FORMULA
     #A=B*C-2*(B+C)
-    D=MATRIX MULTIPLICATION(B,C)
-    E=MATRIX ADDITION(B,C)
-    F=MULTIPLY BY INTEGER(2,E)
-    A=D-FE
+    D<-MATRIX MULTIPLICATION(B,C)
+    E<-MATRIX ADDITION(B,C)
+    F<-MULTIPLY BY INTEGER(2,E)
+    A<-D-FE
+#1+1+1+n+n+n+n+n+n^2+n^3+n+n+n=3+8n+n^2+n^3
+    #O(n^3)
